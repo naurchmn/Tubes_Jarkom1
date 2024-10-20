fc03bb9ef82a2569e145622199fd5f863a343dde
snoeriza <19623022@std.stei.itb.ac.id>
2024-10-20 14:50:09 +0700

    Fix : error handling for port and ip address

--- client.py
+++ client.py
@@ -8,10 +8,39 @@ client.bind(("0.0.0.0", random.randint(8000, 9000)))
 username= input("Username:")
 hostname= socket.gethostname()
 IP = socket.gethostbyname(hostname)
-print(IP)
+print(f"Your IP: {IP}")
 
-sendPORT= int(input("Port: "))
-sendIP=input("IP: ")
+def get_valid_port():
+    while True:
+        try:
+            sendPORT= int(input("Port: "))
+            if 0<= sendPORT<=65535:
+                if sendPORT !=9999:
+                    while True:
+                        pilih = input(f"Port yang anda masukkan bukan port server kami (9999)."
+                                      f"Anda ingin melanjutkan dengan port {sendPORT}? (y/n):").lower()
+                        if pilih == 'n':
+                            return 9999
+                        elif pilih =='y':
+                            return sendPORT
+                        else:
+                            print ("pilihan tidak valid, coba lagi.")
+                    else:
+                        return sendPORT
+                else:
+                    print("Port harus dalam rentang 0-65535, coba lagi.")
+        except ValueError:
+            print("Port yang dimasukkan tidak valid. Harus berupa angka, coba kembali")
+def get_valid_IP():
+    while True:
+        sendIP=input("IP: ")
+        try:
+            socket.inet_aton(sendIP)
+            return sendIP
+        except socket.error:
+            print("IP Address tidak valid, coba lagi")
+sendPORT = get_valid_port()
+sendIP = get_valid_IP()
 server_address = (sendIP, sendPORT)
 
 password = input("Password: ")
